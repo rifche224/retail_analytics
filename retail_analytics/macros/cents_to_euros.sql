24fde7ca320086686a1c9f0ed04ab6a60915afc2
Cherif Amanatoulha Sy <cherifamanatoulhasy@Cherifs-MacBook-Pro.local>
2026-01-28 15:18:57 +0100

    refactor: improve macro configuration with dynamic euro_rate variable for dynamic code

--- retail_analytics/macros/cents_to_euros.sql
+++ retail_analytics/macros/cents_to_euros.sql
@@ -1,4 +1,4 @@
-
 {% macro cents_to_euros(column_name) %}
-    (coalesce({{ column_name }}, 0) / 100.0) * 0.85
+    {% set exchange_rate = var('euro_rate', 0.85) %}
+    (coalesce({{ column_name }}, 0) / 100.0) * {{ exchange_rate }}
 {% endmacro %}
